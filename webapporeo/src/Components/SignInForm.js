7f76896c3a1ead3de3c50816b2506a6578e87e2e
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-05-13 19:49:20 +0530

    Refactored: form to show event erro in input

--- webapporeo/src/Components/SignInForm.js
+++ webapporeo/src/Components/SignInForm.js
@@ -2,35 +2,66 @@ import React from 'react'
 import '../CSS/SignInForm.css'
 import LockRoundedIcon from '@material-ui/icons/LockRounded';
 import AccountCircleSharpIcon from '@material-ui/icons/AccountCircleSharp';
+import { handlPattern } from '../Utility/UsersAuthServices'
 
 
 
-function SignInForm() {
+function SignInForm(props) {
+
+    const [fieldStatus, setFieldStatus] = React.useState({ usernameField: false, emailField: false, passwordField: false })
+    const [fieldValues, setFieldValues] = React.useState({ emailValue : "" , passValue : ""})
+    const testPattern = (e) => {
+        if (e.target.name == "email") {
+            if (handlPattern("email", e.target.value) || e.target.value == "") {
+                setFieldStatus({ ...fieldStatus, emailField: false })
+                setFieldValues({...fieldValues , emailValue : e.target.value})
+            }
+            else if (!handlPattern("email", e.target.value)) {
+                setFieldStatus({ ...fieldStatus, emailField: true })
+            }
+        }
+        else if (e.target.name == "pass") {
+            if (handlPattern("pass", e.target.value) || e.target.value == "") {
+                setFieldStatus({ ...fieldStatus, passwordField: false })
+                setFieldValues({...fieldValues , passValue : e.target.value})
+            }
+            else if (!handlPattern("pass", e.target.value)) {
+                setFieldStatus({ ...fieldStatus, passwordField: true })
+            }
+        }
+    }
+
+    const submitData = () => {
+        console.log(fieldValues)
+    }
+
     return (
         <>
-        <div className = "signInPageFromContainer">
-            <div className = "logoContainerSignInPageForm"> <img src = "https://thememakker.com/templates/oreo/html/assets/images/logo.svg"  className = "logoSignInPageForm"/></div>
-            <div className = "formHeadngContainerSignInPageForm">
-                <h4>Log in</h4>
-            </div>
-            <form className = "mainFormContainerSignInPage">
-                <div className = "inputEmailAndPasswordSignInPageContainer" id = "emailSignInPage" >
-                <input type = "text" placeholder = "Enter User Name"  className = "inputEmailSignInPage"></input>
-                <div  className = "logoEmailAndPassowrdContianer"><AccountCircleSharpIcon style= {{color : "lightgrey"}} /></div>
+            <div className="signInPageFromContainer">
+                <div className="logoContainerSignInPageForm"> <img src="https://thememakker.com/templates/oreo/html/assets/images/logo.svg" className="logoSignInPageForm" /></div>
+                <div className="formHeadngContainerSignInPageForm">
+                    <h4>Log in</h4>
                 </div>
-                <div className = "inputEmailAndPasswordSignInPageContainer" id = "passSignInPage">
-                <input type = "password" placeholder = "Enter Password" className = "inputPasswordSignInPage"  ></input>
-                <div  className = "logoEmailAndPassowrdContianerPassword"><LockRoundedIcon style= {{color : "lightgrey"}} /></div>
+                <form className="mainFormContainerSignInPage">
+                    <div className="inputEmailAndPasswordSignInPageContainer" id="emailSignInPage" >
+                        <input onChange={testPattern} name="email" type="text" placeholder="Enter User Name" className="inputEmailSignInPage"></input>
+                        <div className="logoEmailAndPassowrdContianer"><AccountCircleSharpIcon style={{ color: "lightgrey" }} /></div>
+                    </div>
+                    <div style={{ color: "red" }} >{fieldStatus.emailField ? "Follow Format" : ""}</div>
+                    <div className="inputEmailAndPasswordSignInPageContainer" id="passSignInPage">
+                        <input onChange={testPattern} name="pass" type="password" placeholder="Enter Password" className="inputPasswordSignInPage"  ></input>
+                        <div className="logoEmailAndPassowrdContianerPassword"><LockRoundedIcon style={{ color: "lightgrey" }} /></div>
+                    </div>
+                    <div style={{ color: "red" }}>{fieldStatus.passwordField ? "Follow Format" : ""}</div>
+
+                </form>
+
+                <div className="signInButtonSignInPageContainer">
+                    <button onClick = {submitData} className="signInButtonSignInPage">Sign In</button>
                 </div>
 
-            </form>
-
-            <div className = "signInButtonSignInPageContainer">
-                <button className = "signInButtonSignInPage">Sign In</button>
+                <div className="forgotPasswordContainer"><h4>Forgot Password ?</h4> </div>
             </div>
-
-            <div className = "forgotPasswordContainer"><h4>Forgot Password ?</h4> </div>
-        </div>    
         </>
     )
 }
