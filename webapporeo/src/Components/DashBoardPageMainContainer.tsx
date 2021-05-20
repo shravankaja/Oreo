441b5749cc64b7f06548f923a18e1be6e55bc70f
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-05-20 11:10:42 +0530

    Refactored: parent componenet for enabling conditional rendring of child componenets

--- webapporeo/src/Components/DashBoardPageMainContainer.tsx
+++ webapporeo/src/Components/DashBoardPageMainContainer.tsx
@@ -2,21 +2,43 @@ import '../CSS/DashBoardMainPageContainer.css'
 import DashBoardEcommerce from '../Components/DashboardEcomDashboard'
 import PageHeadingContainer from '../Components/PageHeadingContainer'
 import Product from '../Components/Product'
-export{}
+import ProductList from '../Components/ProductList' 
+import React, {useEffect} from 'react'
+export {}
 
-function DashboardMainPageContainer() {
+interface propsType {
+    productDisplayStatus : any
+}
+
+
+function DashboardMainPageContainer(props:propsType) {
+    const [pageStatus , setPageStatus] = React.useState({displayProductList : false , displayProducts : false, displayDashboard : true, 
+     pageHeading : "eCommerce Dashboard", subHead : "welcome to oreo"})
+   
+    
+    useEffect(() => {
+        if(props.productDisplayStatus == "pl") {
+            setPageStatus({...pageStatus, displayProductList : true, displayDashboard : false,
+                pageHeading : "Product List", subHead : ""})
+        }
+        else if (props.productDisplayStatus == "p") {
+            setPageStatus({...pageStatus, displayProductList : false, displayDashboard : false, displayProducts : true,
+                pageHeading : "Products", subHead : ""})
+        }
+        else if(props.productDisplayStatus == "D") {
+            setPageStatus({...pageStatus, displayDashboard : true, pageHeading : "eCommerce Dashboard", subHead : "welcome to oreo"})
+        }
+    })
+    
+
     return (
         <>
-        <div className = "dashboard-Main-Contianer">
-            <PageHeadingContainer mainHeading = "eCommerce Dashboard" subHeading = "welcome to oreo" />    
-            <Product /> 
+            <div className="dashboard-Main-Contianer">
+                <PageHeadingContainer mainHeading={pageStatus.pageHeading} subHeading={pageStatus.subHead} />
+            {pageStatus.displayDashboard ? <DashBoardEcommerce/> : pageStatus.displayProductList ? <ProductList /> : <Product/> }
+           
             </div>
         </>
-        /*
-        
-         <DashBoardEcommerce />
-           
-        */
     )
 }
 
