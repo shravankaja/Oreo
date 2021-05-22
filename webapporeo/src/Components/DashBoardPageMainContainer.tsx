722bde16edc6d1e164235fd209a9ff2fe1ce5764
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-05-22 06:13:48 +0530

    Refactored: to accomidate product detail componenet

--- webapporeo/src/Components/DashBoardPageMainContainer.tsx
+++ webapporeo/src/Components/DashBoardPageMainContainer.tsx
@@ -2,41 +2,57 @@ import '../CSS/DashBoardMainPageContainer.css'
 import DashBoardEcommerce from '../Components/DashboardEcomDashboard'
 import PageHeadingContainer from '../Components/PageHeadingContainer'
 import Product from '../Components/Product'
-import ProductList from '../Components/ProductList' 
-import React, {useEffect} from 'react'
-export {}
+import ProductList from '../Components/ProductList'
+import ProductDetail from '../Components/ProductDetail'
+import React, { useEffect } from 'react'
+export { }
 
 interface propsType {
-    productDisplayStatus : any
+    productDisplayStatus: any
 }
 
 
-function DashboardMainPageContainer(props:propsType) {
-    const [pageStatus , setPageStatus] = React.useState({displayProductList : false , displayProducts : false, displayDashboard : true, 
-     pageHeading : "eCommerce Dashboard", subHead : "welcome to oreo"})
-   
-    
+function DashboardMainPageContainer(props: propsType) {
+    const [pageStatus, setPageStatus] = React.useState({
+        displayProductList: false, displayProducts: false, displayDashboard: true,
+        pageHeading: "eCommerce Dashboard", subHead: "welcome to oreo", productDetail: false, propsToDetail: 0
+    })
+
+
     useEffect(() => {
-        if(props.productDisplayStatus == "pl") {
-            setPageStatus({...pageStatus, displayProductList : true, displayDashboard : false,
-                pageHeading : "Product List", subHead : ""})
+        console.log(props.productDisplayStatus)
+        if (props.productDisplayStatus == "pl") {
+            setPageStatus(prevState => ({
+                ...prevState, displayProductList: true, displayDashboard: false, displayProducts: false, productDetail: false,
+                pageHeading: "Product List", subHead: ""
+            }));
         }
         else if (props.productDisplayStatus == "p") {
-            setPageStatus({...pageStatus, displayProductList : false, displayDashboard : false, displayProducts : true,
-                pageHeading : "Products", subHead : ""})
+            setPageStatus(prevState => ({
+                ...prevState, displayProductList: false, displayDashboard: false, displayProducts: true, productDetail: false,
+                pageHeading: "Products", subHead: ""
+            }))
         }
-        else if(props.productDisplayStatus == "D") {
-            setPageStatus({...pageStatus, displayDashboard : true, pageHeading : "eCommerce Dashboard", subHead : "welcome to oreo"})
+        else if (props.productDisplayStatus == "D") {
+            setPageStatus(prevState => ({
+                ...prevState, displayDashboard: true, pageHeading: "eCommerce Dashboard", subHead: "welcome to oreo",
+                productDetail: false, displayProducts: false, displayProductList: false
+            }))
         }
-    })
+    },[props.productDisplayStatus])
+
+    const handlEventFromProduct = (p : any, id: boolean) => {
     
+        setPageStatus(prevState => ({ ...prevState, productDetail: true, displayDashboard: false, displayProductList: false, displayProducts: false, propsToDetail : p.id }))
+    }
+
 
     return (
         <>
             <div className="dashboard-Main-Contianer">
                 <PageHeadingContainer mainHeading={pageStatus.pageHeading} subHeading={pageStatus.subHead} />
-            {pageStatus.displayDashboard ? <DashBoardEcommerce/> : pageStatus.displayProductList ? <ProductList /> : <Product/> }
-           
+                {pageStatus.displayDashboard ? <DashBoardEcommerce /> : pageStatus.displayProductList ? <ProductList /> : pageStatus.displayProducts ? <Product listenToClickOnProduct={handlEventFromProduct} /> : <ProductDetail idOfProduct={pageStatus.propsToDetail} />}
+
             </div>
         </>
     )
